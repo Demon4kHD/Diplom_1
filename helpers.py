b5ae104d0691316db5823fc04de80299b64a9536
Dmitriy Demidov <d@demon4k.ru>
2024-10-15 17:01:14 +0300

    Some files have been changed

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 
 def create_data_for_test_database(current_dict: dict, current_key: str):
     len_dict = len(current_dict[current_key])
@@ -7,3 +9,14 @@ def create_data_for_test_database(current_dict: dict, current_key: str):
         list_with_tuple.append(current_tuple)
 
     return list_with_tuple
+
+def create_receipt_for_check(bun_name, list_ingredients, total_price) -> str:
+    receipt: List[str] = [f'(==== {bun_name} ====)']
+
+    for ingredient in list_ingredients:
+        receipt.append(f'= {str(ingredient[0]).lower()} {ingredient[1]} =')
+
+    receipt.append(f'(==== {bun_name} ====)\n')
+    receipt.append(f'Price: {total_price}')
+
+    return '\n'.join(receipt)
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,19 +1,59 @@
-import pytest
-
 from data_burgers import DATA_BUNS
 from praktikum.bun import Bun
+from helpers import create_receipt_for_check
 
 
 class TestBurger:
     def test_add_new_bun_and_set_adding(self, set_bun_in_order):
-        burger, price = set_bun_in_order
-        assert price == DATA_BUNS['prices'][0] * 2
+        burger, price, name = set_bun_in_order
+        burger_price = burger.get_price()
+        bun_price = price * 2
+        assert burger_price == bun_price
 
     def test_changing_the_selection_bun_true(self, set_bun_in_order):
-        burger, price = set_bun_in_order
+        burger, price, name = set_bun_in_order
         bun = Bun(DATA_BUNS['names'][1], DATA_BUNS['prices'][1])
         burger.set_buns(bun)
-        price = burger.get_price()
-        assert price == DATA_BUNS['prices'][1] * 2
+        burger_price = burger.get_price()
+        bun_price = DATA_BUNS['prices'][1] * 2
+        assert burger_price == bun_price
 
+    def test_add_new_ingredient_and_get_price(self, add_ingredient_and_bun_for_test):
+        (burger, bun_price, bun_name,
+         ingredient_type, ingredient_name, ingredient_price, ingredient) = add_ingredient_and_bun_for_test
+        price_burger = burger.get_price()
+        check_value = bun_price * 2 + ingredient_price
+        assert price_burger == check_value
 
+    def test_remove_ingredient_in_burger(self, add_ingredient_and_bun_for_test):
+        (burger, bun_price, bun_name,
+         ingredient_type, ingredient_name, ingredient_price, ingredient) = add_ingredient_and_bun_for_test
+        burger.remove_ingredient(0)
+        price_burger = burger.get_price()
+        check_value = bun_price * 2
+        assert price_burger == check_value
+
+    def test_get_receipt_burger(self, add_ingredient_and_bun_for_test):
+        (burger, bun_price, bun_name,
+         ingredient_type, ingredient_name, ingredient_price, ingredient) = add_ingredient_and_bun_for_test
+        check_price_value = bun_price * 2 + ingredient_price
+        burgers_receipt = burger.get_receipt()
+        check_burger_receipt = create_receipt_for_check(bun_name,
+                                                        [[ingredient_type, ingredient_name]],
+                                                        check_price_value)
+        assert burgers_receipt == check_burger_receipt
+
+    def test_move_ingredient_in_burger(self, add_two_ingredient_and_one_bun_for_test):
+        (burger, bun_price, bun_name, ingredient_type, ingredient_name, ingredient_price, ingredient,
+         second_ingredient_type, second_ingredient_name, second_ingredient_price, second_ingredient) = (
+            add_two_ingredient_and_one_bun_for_test
+        )
+        burger.move_ingredient(1, 0)
+        check_price_value = bun_price * 2 + ingredient_price + second_ingredient_price
+        burgers_receipt = burger.get_receipt()
+        check_burger_receipt = create_receipt_for_check(bun_name,
+                                                        [[second_ingredient_type, second_ingredient_name],
+                                                         [ingredient_type, ingredient_name]
+                                                         ],
+                                                        check_price_value)
+        assert burgers_receipt == check_burger_receipt
